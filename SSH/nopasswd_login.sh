e97c2e5363bf9ed2fbd9a5fa4f81e531112a08aa
Big Bear <2337256919@qq.com>
2017-01-29 00:26:01 +0800

    update

--- SSH/nopasswd_login.sh
+++ SSH/nopasswd_login.sh
@@ -7,12 +7,15 @@
 #local-host
 #below is a simple ,you should change ruser(adviced not be root) ,remote-host and remote-port
 
+#no passwd login setting
 ssh-keygen -t -rsa -P ""
 if [find ~/.ssh/id_rsa.pub];then
 echo "Error to generate the host file"
 exit 0
 fi
 ssh-copy-id -i ~/.ssh/id_rsa.pub ruser@remote-host
+
+#reverse ssh tunnel
 ssh ruser@remotessh -f -N -R remote-port:localhost:22 ruser@remote-host 
 while true;do
 RET=`ps aux | grep "ssh -f -N -R remote-port:localhost:22" | grep -v "grep"`
@@ -22,7 +25,6 @@ ssh -f -N -R remote-port:localhost:22 ruser@remote-host
 fi
 sleep 10
 done
-
 #remote-host
 #to ensure the file /home/ruser/.ssh/authorized_keys has right -rw-------(600)
 #and the remote-port is avaliable
