e7c6e20bbb99ee5fbb9038c74aa90d9635f6ede4
zm <2337256919@qq.com>
2017-03-04 16:35:36 +0800

    2

---
+++ GreenGraphrun.sh
@@ -0,0 +1,25 @@
+##
+# Author: kingcc<laikinfox@gmail.com>
+# Date  : 2016/10/1
+##
+
+#!/bin/bash
+
+###Redesign for myself :)
+cd ~/MyGit/ShellScript/
+eval "$(ssh-agent -s)"
+ssh-add
+commits=$(($RANDOM%20+1))
+i=0
+while(($i<$commits))
+do
+commitTimesp=`sed -n '/run/p' ./README.md`
+commitTimesh=${commitTimesp#*run }
+commitTimes=${commitTimesh% times.}
+sed -i "s/$commitTimes/$(($commitTimes+1))/" ./README.md
+git add -A && git commit -m "$(($commitTimes+1))"
+i=$(($i+1))
+done
+git push origin master
+pkill -9 ssh-agent
+cd ~
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ShellScript
 Some useful common shell scripts
+# GreenGraph 
+This script has run 2 times.
