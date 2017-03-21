19e4b047be36787ae2a019f92dd9362de74c29a5
zm <2337256919@qq.com>
2017-03-21 01:21:38 -0400

    112

--- GreenGraphrun.sh
+++ GreenGraphrun.sh
@@ -10,7 +10,8 @@
 DIR="$( cd "$( dirname "$0"  )" && pwd  )"
 PRO=$DIR/GreenGraphrun.sh
 LOG=/home/zmvps/git.log
-CMD=00 00 * * * $PRO>>$LOG 2>&1
+CMD="00 00 * * * $PRO>>$LOG 2>&1 &"
+echo $CMD
 cd $DIR
 eval "$(ssh-agent -s)"
 ssh-add
@@ -26,6 +27,6 @@ git add -A && git commit -m "$(($commitTimes+1))"
 i=$(($i+1))
 done
 git push origin master
-(crontab -l 2>/dev/null | grep -Fv $PRO; echo $CMD) | crontab -
+(crontab -l 2>/dev/null | grep -Fv $PRO; echo "$CMD") | crontab -
 sudo pkill -8 ssh-agent
 cd ~
